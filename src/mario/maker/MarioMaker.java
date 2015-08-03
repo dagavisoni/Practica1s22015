ece2a2f379f25eda99161bf799829a01bd505b77
Yordi <Yordi@Yordi-PC>
2015-08-02 20:56:55 -0600

    Practica edd a github

--- src/mario/maker/MarioMaker.java
+++ src/mario/maker/MarioMaker.java
@@ -16,6 +16,11 @@ public class MarioMaker {
      */
     public static void main(String[] args) {
     System.out.println("hoadsasdasd");
+    /****
+     * 
+     * 
+     * sddds
+     */
     }
     
 }
